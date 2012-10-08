d400a06501f4a69b5c1917d8f7dec7d8ec239698
Javier Goizueta <jgoizueta@gmail.com>
2012-10-08 09:07:05 +0200

    Basic sanity test

--- test/test_solar.rb
+++ test/test_solar.rb
@@ -1,3 +1,15 @@
 require 'helper'
 
-# TODO: write tests!!
+class TestSolar < Test::Unit::TestCase
+
+  should "calculate transits for the required date" do
+    d = Date.new(2012,10,7)
+    for lon in -360..360
+      for lat in -50..50
+        r,trans,s = Solar.passages(d, lon, lat)
+        assert_equal d, trans.utc.to_date, "longitude: #{lon} latitude: #{lat}"
+      end
+    end
+  end
+
+end
